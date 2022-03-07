73df6f8d1d83120b573393fab079d7fa33afd959
nikisha-script <nikisha_script@mail.ru>
2022-03-07 20:39:17 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,40 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+
+    private static int countEngine = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        if ("A380".equals(this.name)) {
+            countEngine = 4;
+        }
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+
+}
