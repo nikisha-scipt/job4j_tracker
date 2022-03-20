a8a3a330a67760eba466746c07808e6f977c6b95
nikisha-script <nikisha_script@mail.ru>
2022-03-20 15:23:24 +0300

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        return left.compareTo(right);
+    }
+
+}
