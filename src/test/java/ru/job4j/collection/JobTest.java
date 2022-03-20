d9c17c248f11c1f53f1c0ab9f6fedc8757af4389
nikisha-script <nikisha_script@mail.ru>
2022-03-20 14:18:51 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,94 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAsc() {
+        Comparator<Job> ascComp = new JobAscByName();
+        int res = ascComp.compare(
+                new Job("test1", 0),
+                new Job("test2", 2)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByPriorityAsc() {
+        Comparator<Job> ascComp = new JobAscByPriority();
+        int res = ascComp.compare(
+                new Job("test1", 0),
+                new Job("test2", 2)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByPriorityDesc() {
+        Comparator<Job> ascComp = new JobDescByPriority();
+        int res = ascComp.compare(
+                new Job("test2", 2),
+                new Job("test1", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameDesc() {
+        Comparator<Job> ascComp = new JobDescByName();
+        int res = ascComp.compare(
+                new Job("test2", 2),
+                new Job("test1", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByJobDescByNameLn() {
+        Comparator<Job> ascComp = new JobDescByNameLn();
+        int res = ascComp.compare(
+                new Job("test22", 2),
+                new Job("test1", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAscAndPriorityAsc() {
+        Comparator<Job> ascComp = new JobAscByName().thenComparing(new JobAscByPriority());
+        int res = ascComp.compare(
+                new Job("test1", 0),
+                new Job("test2", 2)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAll() {
+        Comparator<Job> ascComp = new JobAscByName()
+                .thenComparing(new JobAscByPriority())
+                .thenComparing(new JobDescByName())
+                .thenComparing(new JobDescByNameLn())
+                .thenComparing(new JobDescByPriority());
+        int res = ascComp.compare(
+                new Job("test1", 0),
+                new Job("test2", 2)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+}
