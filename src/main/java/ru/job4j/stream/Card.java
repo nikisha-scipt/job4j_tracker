a31de067aaabfecab18d2083658db8794e0638f6
nikisha-scipt <nikisha_script@mail.ru>
2022-04-01 21:35:59 +0300

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Suit[] suits = {Suit.Diamonds, Suit.Hearts, Suit.Spades, Suit.Clubs};
+        Value[] values = {Value.V_6, Value.V_7, Value.V_8, Value.V_6};
+        Stream.of(suits)
+                .flatMap(suit1 -> Stream.of(values)
+                        .map(value -> suit1 + " " + value))
+                .forEach(System.out::println);
+
+    }
+}
